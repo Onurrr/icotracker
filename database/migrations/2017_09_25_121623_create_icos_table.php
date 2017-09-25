c1eb7addf3b71e2e7e2973318d0e9a32dfce7adb
Onur Ozdemir <onur38o@hotmail.com>
2017-09-25 14:59:47 +0200

    Create icos table migration

---
+++ database/migrations/2017_09_25_121623_create_icos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIcosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('icos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('name');
+            $table->text('website');
+            $table->text('symbol');
+            $table->text('body');
+            $table->date('start');
+            $table->date('end')->nullable();
+            $table->integer('total_supply');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('icos');
+    }
+}
