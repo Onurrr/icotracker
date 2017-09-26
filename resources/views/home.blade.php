07fb178b38fac6f6a1b7e19f48c369a92087a079
Onur Ozdemir <onur38o@hotmail.com>
2017-09-27 00:17:27 +0200

    Display Ico data on homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,5 +19,19 @@
             </div>
         </div>
     </div>
+    <div class="row">
+        @foreach ($icos as $ico)
+          <div class="col-6 col-md-4">
+            <div class="thumbnail">
+      <img src="http://www.canbike.org/public/images/030114/Bitcoin_Logo_Vertical.png" style="max-height:120px;">
+      <div class="caption">
+        <h3>{{$ico->name}}</h3>
+        <p>{{$ico->body}}</p>
+        <p><a href="#" class="btn btn-primary" role="button">More</a>
+      </div>
+    </div>
+          </div>
+        @endforeach
+    </div>
 </div>
 @endsection
