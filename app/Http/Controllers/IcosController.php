12597eac6e0af265a0cab706c292650de7b0e9d2
Onur Ozdemir <onur38o@hotmail.com>
2017-09-27 17:39:03 +0200

    Created IcosController

---
+++ app/Http/Controllers/IcosController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Ico;
+
+class IcosController extends Controller
+{
+	  /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+    	$icos = Ico::all();
+
+
+    	return view('coins.index', compact('icos'));
+    }
+
+    public function show($id)
+    {
+    	$ico = Ico::find($id);
+
+    	return view('coins.show', compact('ico'));
+    }
+}
