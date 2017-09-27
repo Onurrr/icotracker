a28c11003b48478a91b112b68594c365c58f69de
Onur Ozdemir <onur38o@hotmail.com>
2017-09-27 17:38:51 +0200

    Removed unnecessary parts from homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Ico;
 
 class HomeController extends Controller
 {
@@ -24,7 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $icos = Ico::all();
-        return view('home',compact('icos'));
+        return view('home');
     }
 }
