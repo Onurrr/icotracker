e108c6c7ab6eb92d367a47f66998be9c735c99af
Onur Ozdemir <onur38o@hotmail.com>
2017-09-27 23:11:55 +0200

    Styled detailpages

--- resources/views/coins/show.blade.php
+++ resources/views/coins/show.blade.php
@@ -7,8 +7,13 @@
             <div class="thumbnail">
       <img src="http://www.canbike.org/public/images/030114/Bitcoin_Logo_Vertical.png" style="max-height:120px;">
       <div class="caption">
-        <h3>{{$ico->name}}</h3>
+        <h1>{{$ico->name}}</h1>
+        <p>Ticker: <span class="label label-default">{{$ico->symbol}}</span></p>
+        <span class="label label-primary">Mineable</span>
+        <span class="label label-primary">Coin</span>
+        <span class="label label-primary">Token</span>
         <p>{{$ico->body}}</p>
+        <p><a href="//{{$ico->website}}" target="_blank" class="btn btn-primary" role="button">Website</a></p>
       </div>
     </div>
           </div>
