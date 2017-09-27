68fa13280f8d1886dfd4e5d2fbc97a4d5eb3df5e
Onur Ozdemir <onur38o@hotmail.com>
2017-09-27 21:30:32 +0200

    Added coins to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
             <div class="container">
                 <div class="navbar-header">
 
+
                     <!-- Collapsed Hamburger -->
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
@@ -36,7 +37,7 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="/coins">Coins</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
